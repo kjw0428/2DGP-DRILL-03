12270c2c648758565ad98461ec8c3af2c9b6e38a
kjw0428 <naver.com@tukorea.ac.kr>
2025-09-19 20:19:11 +0900

    삼각운동 추가

--- character_moves.py
+++ character_moves.py
@@ -38,3 +38,31 @@ while True:
         character.draw_now(x, y)
         x += 2
         delay(0.01)
+    ax, ay = 400, 90
+    bx, by = 150, 400
+    cx, cy = 650, 400
+    steps = 150
+    for i in range(steps):
+        t = i / steps
+        x = int(ax + (bx - ax) * t)
+        y = int(ay + (by - ay) * t)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.01)
+    for i in range(steps):
+        t = i / steps
+        x = int(bx + (cx - bx) * t)
+        y = int(by + (cy - by) * t)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.01)
+    for i in range(steps):
+        t = i / steps
+        x = int(cx + (ax - cx) * t)
+        y = int(cy + (ay - cy) * t)
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        delay(0.01)
